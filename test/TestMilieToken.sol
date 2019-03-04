5def26565775d62985ae1717497d747cdee9a346
vmahavadi <vidyamahavadi@Rams-MacBook-Air.local>
2019-03-04 22:52:39 +0200

    First copy

---
+++ test/TestMilieToken.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/milieToken.sol";
+
+contract TestMilieToken {
+ // The address of the adoption contract to be tested
+ milieToken milie = milieToken();
+
+ 
+
+}
